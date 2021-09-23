9e9b20d9f413af0ce2b8d05bd0f104a17f229ca3
Thandomac1 <90968046+Thandomac1@users.noreply.github.com>
2021-09-23 15:55:20 +0200

    Updated version of Hero game

--- The Hero Game/Form1.Designer.cs
+++ The Hero Game/Form1.Designer.cs
@@ -40,7 +40,7 @@ namespace The_Hero_Game
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+           
             // 
             // Form1
             // 
--- The Hero Game/Form1.cs
+++ The Hero Game/Form1.cs
@@ -9,168 +9,214 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace The_Hero_Game
-{
-   public abstract class Tile //Question 2.1
+{   
+    public partial class Form1 : Form
     {
-        protected int X;
-        protected int Y;
+        public abstract class Tile //Question 2.1
+        {
+            protected int X;
+            protected int Y;
 
-        public enum Tiletype
-        {
-            Hero,
-            Enemy,
-            Gold,
-            Weapon,
-        }
-        public Tile()
-        {
-
-        }
-        public Tile(int x, int y)
-        {
-            X = x;
-            Y = y;
-        }
-    }
-    public abstract class Obstacle : Tile
-    {
-        public Obstacle()
-        {
-
-        }
-        public Obstacle(int x, int y)
-        {
-            this.X = x;
-            this.Y = y;
-        }
-    }
-    public abstract class EmptyTile : Tile
-    {
-        
-    }
-    public abstract class Character : Tile //Question 2.2
-    { 
-        protected int Hp;
-        protected int max_Hp;
-        protected int Damage; 
-        char Tile_Border = 'X';
-        string[] characterVision = new string[] { "north", "south", "west", "East" };
-       
-        public Character()
-        {
-
-        }
-    
-        public Character(int hp, int max_hp, int damage)
-        {
-            Hp = hp;
-            max_Hp = max_hp;
-            Damage = damage;
-        }
-        public Character(int x, int y, char tile_border)
-        {
-            this.X = x;
-            this.Y = y;
-            Tile_Border = tile_border;
-        }
-        public enum Movement
-        {
-            No_movement,
-            up,
-            Down,
-            Left,
-            Right,
-        }
-        public virtual void Attack()
-        {
-            
-        }
-        public bool IsDead()
-        {
-            return false;
-        }
-        public virtual bool CheckRange()
-        {
-            return false;
-        }
-        private int toDistance()
-        {
-            return 0;
-        }
-        public abstract Movement returnMove(Movement move);
-        public void move()
-        {
-
-        }
-        public abstract override string ToString();
-        //public abstract movementEnum();
-        public abstract class Enemy : Character
-        {
-            protected Random r = new Random();
-            public Enemy()
+            public enum Tiletype
+            {
+                Hero,
+                Enemy,
+                Gold,
+                Weapon,
+            }
+            public Tile()
             {
 
             }
-            public Enemy(int x, int y, int damage, int hp, int max_Hp)
+            public Tile(int x, int y)
             {
-
-            }
-            public override string ToString()
-            {
-                return ("Enemy goblin at" + X + Y + Damage);
+                X = x;
+                Y = y;
             }
         }
-        public class Goblin : Enemy
+        public abstract class Obstacle : Tile
         {
-            char goblin = 'G';
-            public Goblin()
+            public Obstacle()
             {
 
             }
-            public Goblin(int x, int y)
+            public Obstacle(int x, int y)
             {
                 this.X = x;
                 this.Y = y;
-                this.max_Hp = 10;
-                this.Damage = 1;
-            }
-            public override Movement returnMove(Movement move)
-            {
-                r.Next(1, 5);
-                return move;
             }
         }
-        public class Hero : Character
+        public abstract class EmptyTile : Tile
         {
-            char hero = 'H';
-            public Hero(int x, int y, int hp, int max_hp) 
+
+        }
+        public abstract class Character : Tile //Question 2.2
+        {
+            protected int Hp;
+            protected int max_Hp;
+            protected int Damage;
+            char Tile_Border = 'X';
+            string[] characterVision = new string[] { "north", "south", "west", "East" };
+
+            public Character()
+            {
+
+            }
+
+            public Character(int hp, int max_hp, int damage)
+            {
+                Hp = hp;
+                max_Hp = max_hp;
+                Damage = damage;
+            }
+            public Character(int x, int y, char tile_border)
             {
                 this.X = x;
                 this.Y = y;
-                this.Hp = hp;
-                this.max_Hp = max_hp;
-                this.Damage = 2;
+                Tile_Border = tile_border;
+            }
+            public enum Movement
+            {
+                No_movement,
+                up,
+                Down,
+                Left,
+                Right,
+            }
+            public virtual void Attack()
+            {
+
+            }
+            public bool IsDead()
+            {
+                return false;
+            }
+            public virtual bool CheckRange()
+            {
+                return false;
+            }
+            private int toDistance()
+            {
+                return 0;
+            }
+            public abstract Movement returnMove(Movement move);
+            public void move()
+            {
+
+            }
+            public abstract override string ToString();
+            //public abstract movementEnum();
+            public abstract class Enemy : Character
+            {
+                protected Random r = new Random();
+                public Enemy()
+                {
+
+                }
+                public Enemy(int x, int y, int damage, int hp, int max_Hp)
+                {
+
+                }
+                public override string ToString()
+                {
+                    return ("Enemy goblin at" + X + Y + Damage);
+                }
+            }
+            public class Goblin : Enemy
+            {
                 
+                public Goblin()
+                {
+
+                }
+                public Goblin(int x, int y)
+                {
+                    this.X = x;
+                    this.Y = y;
+                    this.max_Hp = 10;
+                    this.Damage = 1;
+                    char goblin = 'G';
+                }
+                public override Movement returnMove(Movement move)
+                {
+                   move = (Movement)r.Next(1, 5);
+                    return move;
+                }
             }
-            public override Movement returnMove(Movement move)
+            public class Hero : Character
             {
-              
-                return move;
+                
+                public Hero(int x, int y, int hp, int max_hp)
+                {
+                    this.X = x;
+                    this.Y = y;
+                    this.Hp = hp;
+                    this.max_Hp = max_hp;
+                    this.Damage = 2;
+                    char hero = 'H';
+                }
+                public override Movement returnMove(Movement move)
+                {
+
+                    return move;
+                }
+                public override string ToString()
+                {
+                    return ("Player stats:" + "\n" + "Hp:" + Hp + max_Hp + "\n" + "Damage:" + Damage + "\n" + "[" + X + "," + Y + "]");
+                }
             }
-            public override string ToString()
+            public class Map
             {
-                return ("Player stats:" + "\n" + "Hp:" + Hp+ max_Hp + "\n" + "Damage:" + Damage + "\n" + "[" + X + ","+ Y + "]");
+                private int[,] map = new int[0,0];
+                private string hero;
+                private int[] enemies;
+                private  int mapHeight;
+                private  int mapWidth;
+                Random obj = new Random();
+
+                public Map(int min_height, int max_height, int min_width, int max_width, int[] numEnemies)
+                {
+                    mapHeight = randomize(min_height, max_height);                   
+                    mapWidth = randomize(min_width, max_width);
+                    enemies = numEnemies;
+                    
+                }
+                public int randomize(int min, int max)
+                {
+                    int heightmin;
+                    heightmin = obj.Next(min, max);
+                    return heightmin;
+
+                }
+                public void Create()
+                {
+
+                }
+                public void updateVision()
+                {
+
+                }
+               
+            }
+            public class GameEngine
+            {
+                private int map { get; }
+
+                public GameEngine()
+                {
+
+                }
+                public bool MovePlayer(Movement direction )
+                {
+                    return false;
+                }
             }
         }
-        public 
-        public partial class Form1 : Form
-    {
         public Form1()
         {
             InitializeComponent();
-                
-        }
 
+        }
         private void button1_Click(object sender, EventArgs e)
         {
 
